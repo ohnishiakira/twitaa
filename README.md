a79a2ab5643cb6390438af47ebc1eaa00f2f42e1
Akira Ohnishi <s06206ao@gmail.com>
2011-07-24 02:21:02 +0900

    作った

---
+++ README.md
@@ -0,0 +1,6 @@
+# twitaa
+Throw ASCII Art to twitaa.in
+
+# 使い方
+* AAを選択して右クリック
+* twitaa.inを選択
---
+++ twitaa.js
@@ -0,0 +1,28 @@
+function createTab(url) {
+  chrome.tabs.create({url: url});
+}
+
+function toTwitAA(aa) {
+  var formdata = new FormData();
+  var xhr = new XMLHttpRequest();
+
+  formdata.append('asciiart', aa);
+
+  xhr.open("POST", "http://www.twitaa.in/post");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      console.log(xhr.responseText);
+      createTab(xhr.responseText.match(new RegExp("http://twitaa.in/\\?v=.[^\"]*"))[0]);
+    }
+  }
+  xhr.send(formdata);
+}
+
+chrome.contextMenus.create({
+  title: "twitaa.in",
+  contexts: ["selection"],
+  onclick: function(info, tab) {
+    console.log("selection text: " + info.selectionText);
+    toTwitAA(info.selectionText);
+  }
+});
